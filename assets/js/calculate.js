669a5f76c4616e72d8102cd7dd65d848900147fa
Evander Inácio <evander.20112@hotmail.com>
2022-01-28 22:22:07 -0300

    Add files via upload

---
+++ assets/js/calculate.js
@@ -0,0 +1,120 @@
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operator]');
+const equalsButtons = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const allClearButton = document.querySelector('[data-all-clears]');
+const previousButtonsText = document.querySelector('[data-previous]');
+const currentButtonsText = document.querySelector('[data-current]');
+
+class Calculator {
+  constructor(previousButtonsText, currentButtonsText) {
+    this.previousButtonsText = previousButtonsText;
+    this.currentButtonsText = currentButtonsText;
+    this.clear();
+  }
+
+  calculate() {
+    let  result;
+
+    const _previousOperand = parseFloat(this.previousOperand);
+    const _currentOperand = parseFloat(this.currentOperand);
+
+    if (isNaN(_previousOperand) || isNaN(_currentOperand)) return;
+
+    switch (this.operation) {
+      case "+":
+        result = _previousOperand + _currentOperand;
+        break;
+      case "-":
+        result = _previousOperand - _currentOperand;
+        break;
+      case "/":
+        result = _previousOperand / _currentOperand;
+        break;
+      case "*":
+        result = _previousOperand * _currentOperand;
+        break;
+      default:
+        return;
+    }
+
+    this.currentOperand = result;
+    this.operation = undefined;
+    this.previousOperand = "";
+     
+  }
+  
+
+  choseOperation(operation){
+ 
+    if(this.currentOperand === "") return
+
+    if (this.previousOperand !== "") {
+      this.calculate()
+    }
+
+    this.operation = operation
+    this.previousOperand = this.currentOperand;
+    this.currentOperand = ""; 
+  }
+
+  appendNumber(number) {
+    if(this.currentOperand.includes(".") && number === ".") return;
+
+    this.currentOperand = `${this.currentOperand}${number.toString()}`;
+  }
+
+  clear() {
+    this.currentOperand = "";
+    this.previousOperand = "";
+    this.operation = undefined;
+  }
+
+  delete(){
+    this.currentOperand = this.currentOperand.toString().slice(0, -1);
+  }
+
+  updateDisplay() { 
+    this.previousButtonsText.innerText = `${this.previousOperand} ${this.operation || ""}`;
+    this.currentButtonsText.innerText = this.currentOperand;
+  }
+}
+
+const calculator = new Calculator(
+  previousButtonsText,
+  currentButtonsText
+);
+
+
+for(const numberButton of numberButtons){
+  numberButton.addEventListener('click' , () => {
+     calculator.appendNumber(numberButton.innerText);
+     calculator.updateDisplay();
+
+  });
+}
+
+for(const operationButton of operationButtons){
+  operationButton.addEventListener('click', () => {
+    calculator.choseOperation(operationButton.innerText);
+    calculator.updateDisplay();
+  });
+}
+
+// Button AC
+allClearButton.addEventListener('click', () => {
+  calculator.clear();
+  calculator.updateDisplay();
+});
+
+
+deleteButton.addEventListener('click' , () => {
+  calculator.delete();
+  calculator.updateDisplay();
+})
+
+// Result
+equalsButtons.addEventListener('click', () => {
+    calculator.updateDisplay();
+    calculator.calculate();
+} )
---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+
+const Switch_mode = document.getElementById('switch');
+const Inner_text = document.getElementById('inner-text');
+
+mode.onclick = function(){
+  document.body.classList.toggle('sun-theme');
+  Switch_mode.classList.toggle('switch-active');
+  Inner_text.classList.toggle('inner-text-active');
+  if(document.body.classList.toggle('.sun-theme')){
+    Inner_text.innerHTML = 'Dark'
+  }
+  else{
+    Inner_text.innerHTML = 'Light'
+  }
+}
